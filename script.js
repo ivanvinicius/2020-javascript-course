cea9f7946d756bb7c351e34d04d59dad1a1b664f
ivanvinicius <ivanvinicius88@gmail.com>
2020-01-08 20:46:50 -0300

    feat: functions

---
+++ script.js
@@ -0,0 +1,21 @@
+/**FUNCTION STATEMENTS AND EXPRESSIONS */
+
+//function declaration
+function escrevaNome(nome = 'ivan'){
+  //code
+}
+
+//function expression
+var whatDoYouDo = function(nome = 'ivan'){
+  switch(name){
+    case 'ivan' :
+      return true;
+    default : 
+      return false;
+  }
+}
+
+/**
+ * the diference between these two function is
+ * the first one dont need to return a reponse imediately as the second one 
+ */
