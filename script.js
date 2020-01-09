fbd3e459b3d77c3647fce77aa0665c18245c8c83
ivanvinicius <ivanvinicius88@gmail.com>
2020-01-09 14:28:05 -0300

    feat: loops, continue, break

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/**FUNCTION STATEMENTS AND EXPRESSIONS */
+/**FUNCTION STATEMENTS AND EXPRESSIONS ------------------------------------------------------------*/
 
 //function declaration
 function escrevaNome(nome = 'ivan'){
@@ -22,7 +22,7 @@ var whatDoYouDo = function(nome = 'ivan'){
  */
 
 
- /**ARRAYS */
+ /**ARRAYS ---------------------------------------------------------------------------------*/
  var names = ['ivan', 'boneti'];
  var years = new Array(1990, 1991, 1995);
 
@@ -46,7 +46,7 @@ years.indexOf(1991);
 years.indexOf(1998) === -1 ? 'Ivan borned in another date ' :  'Ivan borned in 1998';
 
 
- /**OBJECTS AND PROPERTIES */
+ /**OBJECTS AND PROPERTIES ---------------------------------------------------------*/
 
 /**Defining an object literal */
 var ivan = {
@@ -83,4 +83,37 @@ var luiz = {
 
 /**Call object method and show */
 luiz.calcAge();
-console.log(luiz);
+//console.log(luiz);
+
+/**LOOPS AND INTERATIONS --------------------------------------------------------------------*/
+
+for (let i = 1; i <= 10; i += 2) {
+  //console.log(i);
+}
+
+let j = 1;
+while (j  <= 10) {
+  //console.log(j);
+
+  j++;
+}
+
+/**Continue and Break statement */
+var words = new Array('my', 'name', 'is', 1998, 'ivan');
+
+for (let k = 0; k < words.length; k++) {
+  if(typeof words[k] !== 'string') continue;
+  
+  //console.log(words[k]);
+}
+
+for (let l = 0; l < words.length; l++) {
+  if(typeof words[l] !== 'string') break;
+  
+  //console.log(words[l]);
+}
+
+//Looping backwards
+for (let l = words.length - 1 ; l >=0; l--) {
+  //console.log(words[l]);
+}
