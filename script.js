adbabbfcd402e15fd1ef4f23833554565aba821d
ivanvinicius <ivanvinicius88@gmail.com>
2020-01-09 09:06:24 -0300

    feat: arrays

--- script.js
+++ script.js
@@ -17,5 +17,32 @@ var whatDoYouDo = function(nome = 'ivan'){
 
 /**
  * the diference between these two function is
- * the first one dont need to return a reponse imediately as the second one 
+ * the first one dont need to return a reponse immediately as the second one.
+ * And expression always produce a return value
  */
+
+
+ /**ARRAYS */
+ var names = ['ivan', 'boneti'];
+ var years = new Array(1990, 1991, 1995);
+
+ /**How to insert a new element at first position */
+ names.unshift('lucas');
+
+ /**How to insert a new element at last position  */
+ names[names.length] = 'last value';
+ names.push('vinicius');
+ 
+/**How to delete the first element */
+names.shift();
+
+/**How to delete the last element */
+names.pop();
+
+/**How to get the position of an element */
+years.indexOf(1991);
+
+/**Identifying if element is not in array */
+years.indexOf(1998) === -1 ? 'Ivan borned in another date ' :  'Ivan borned in 1998';
+
+
