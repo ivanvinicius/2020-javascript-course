cd816b42b9d5975bc02df9f0dd0073190df9610b
ivanvinicius <ivanvinicius88@gmail.com>
2020-01-15 21:55:17 -0300

    feat: prototype and inheritance

---
+++ Section 5: Advanced JavaScript objects and functions/script.js
@@ -0,0 +1,66 @@
+/**
+ * NOTES
+ * 
+ * Every Javascript object has a prototype property, which makes inheritance 
+ * possible in Javascript
+ * 
+ * The prototype property of an object is where we put methods and propeties 
+ * that we want other objects to inherit
+ * 
+ * The Constructor's prototype is NOT the prototype of the Constructor itself, 
+ * it's the prototype of all instances that are created through it
+ * 
+ * When a certain method or property is called, the search starts in the object
+ * itself, and if it cannot be found, the search moves on to the object's prototype.
+ * This continues until the methods is found: prototype chain
+ * 
+ */
+
+/**
+ * CONSTRUCTOR FUNCTION
+ *  
+ * In Javascript convention constructors are created with initial capital letter
+ */
+var Person = function(name, yearOfBirth, job){
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+}
+
+/**
+ * Adding new methods or properties inside the constructor function. 
+ * It's called INHERITANCE
+*/
+Person.prototype.calculateAge = function(){
+  console.log(2020 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = function(lastName){
+  this.lastName = lastName;
+}
+
+Person.prototype.city = 'Rio do sul';
+
+console.log(ivan.city);
+
+/**
+ * Creating new person object
+ *
+ * The NEW operador specifies that a new context(IVAN) needs to be created, this way
+ * the THIS variable will not point to GLOBAL context. To check it run the commads 
+ * below.
+ * 
+ * console.log(Person); 
+ * console.log(ivan); 
+ */
+var ivan = new Person('Ivan', 1998, 'programmer');
+
+var mark = new Person('Mark', 1996, 'software engineer');
+
+ivan.lastName('Boneti')
+mark.lastName('Smith');
+
+console.log(ivan);
+console.log(mark);
+
+
