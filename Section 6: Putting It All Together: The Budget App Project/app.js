e4a3bee87dc2f8901ce07b155649251c57305938
ivanvinicius <ivanvinicius88@gmail.com>
2020-02-02 21:01:44 -0300

    feat: Reading input data

--- Section 6: Putting It All Together: The Budget App Project/app.js
+++ Section 6: Putting It All Together: The Budget App Project/app.js
@@ -1,21 +1,46 @@
 //module pattern using IIFE and closures
 
-var budgetController = (function() {
+var budgetController = (() => {
 
   return {}
 })();
 
 
-var UIController = (function() {
-
-  return {}
-})();
 
 
-var controller = (function(budgetCtrl, UICtrl) {
+var UIController = (() => {
   
-  var ctrlAddItem = function() {
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    btnAdd: '.add__btn',
+  }
+
+  return {
+    getInput: () => {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      }
+    },
+
+    getDOMstrings: () => {
+      return DOMstrings;
+    },
+  };
+})();
+
+
+
+
+var controller = ((budgetCtrl, UICtrl) => {
+  var DOMstrings = UICtrl.getDOMstrings();
+
+  var ctrlAddItem = () => {
     //get input data
+    var input = UICtrl.getInput();
 
     //add the item to the budget controller
 
@@ -26,7 +51,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     //display the budget on the UI
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOMstrings.btnAdd).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', (event) => {
     if(event === 13 || event.which === 13) {
