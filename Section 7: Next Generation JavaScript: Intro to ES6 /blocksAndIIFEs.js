05ee464db825d9475eb55ff2f05160f39f518e15
ivanvinicius <ivanvinicius88@gmail.com>
2020-02-10 16:16:08 -0300

    feat: Blocks and IIFEs

--- Section 7: Next Generation JavaScript: Intro to ES6 /blocksAndIIFEs.js
+++ Section 7: Next Generation JavaScript: Intro to ES6 /blocksAndIIFEs.js
@@ -1,1 +1,30 @@
 //& Blocks and IIFEs
+
+/**ES5 */
+var scope = (function() {
+  var d = 4;
+
+  return {
+    getReturn: function() {
+      console.log(d);
+    },
+  }
+})()
+
+//console.log(d); ERROR
+scope.getReturn();
+
+
+
+
+
+/**ES6 */
+{
+  const a = 1;
+  let b = 2;
+  var c = 3;
+}
+
+// console.log(a); ERROR 
+// console.log(b); ERROR
+console.log(c);
